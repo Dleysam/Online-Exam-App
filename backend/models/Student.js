7c61630fbb7d70b788265606de3d0338e2cc46e7
Dleysam <olaleyesamson2016@gmail.com>
2025-11-22 13:47:25 +0100

    Update Student.js

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
 
+const studentSchema = new mongoose.Schema({
+    matric: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    level: { type: String },
+    status: { type: String, default: "active" }
+});
+
+module.exports = mongoose.model("Student", studentSchema);
