ae372a14049ebb8fff9e3effad7757b52349b48b
Dleysam <olaleyesamson2016@gmail.com>
2025-11-22 13:48:04 +0100

    Update Admin.js

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
+const adminSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true }
+});
+
+adminSchema.methods.isValidPassword = async function(password) {
+    return await bcrypt.compare(password, this.passwordHash);
+};
+
+module.exports = mongoose.model("Admin", adminSchema);
