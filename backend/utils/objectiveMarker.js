92b15723897616dff7a585fe9f7eb097ac9c97dd
Dleysam <olaleyesamson2016@gmail.com>
2025-11-22 14:18:35 +0100

    Update objectiveMarker.js

--- backend/utils/objectiveMarker.js
+++ backend/utils/objectiveMarker.js
@@ -1,1 +1,12 @@
+// objectiveMarker.js
+function markObjective(studentAnswers, correctAnswers) {
+    let score = 0;
+    studentAnswers.forEach((ans, idx) => {
+        if(ans.trim().toLowerCase() === correctAnswers[idx].trim().toLowerCase()){
+            score++;
+        }
+    });
+    return score;
+}
 
+module.exports = markObjective;
