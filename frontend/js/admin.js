6e3ad7ed6c28dea5c6c74bd7c9f61f0ef66a0782
Dleysam <olaleyesamson2016@gmail.com>
2025-11-22 14:08:58 +0100

    Update admin.js

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,1 +1,26 @@
+// admin.js
+import { uploadObjective, uploadTheory } from "../js/api.js";
 
+const uploadObjForm = document.getElementById("upload-objective-form");
+if(uploadObjForm){
+    uploadObjForm.addEventListener("submit", async (e)=>{
+        e.preventDefault();
+        const examId = document.getElementById("exam-id").value;
+        const questions = JSON.parse(document.getElementById("questions").value);
+        const token = localStorage.getItem("token");
+        const res = await uploadObjective(token, examId, questions);
+        alert(res.message);
+    });
+}
+
+const uploadTheoryForm = document.getElementById("upload-theory-form");
+if(uploadTheoryForm){
+    uploadTheoryForm.addEventListener("submit", async (e)=>{
+        e.preventDefault();
+        const examId = document.getElementById("exam-id").value;
+        const answers = JSON.parse(document.getElementById("answers").value);
+        const token = localStorage.getItem("token");
+        const res = await uploadTheory(token, examId, answers);
+        alert(res.message);
+    });
+}
