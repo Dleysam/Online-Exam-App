803d4b56343c03ba7eaece336513febb735403b5
Dleysam <olaleyesamson2016@gmail.com>
2025-11-22 13:49:25 +0100

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { studentLogin, adminLogin } = require("../controllers/authController");
 
+// Student login with matric
+router.post("/student-login", studentLogin);
+
+// Admin login
+router.post("/admin-login", adminLogin);
+
+module.exports = router;
